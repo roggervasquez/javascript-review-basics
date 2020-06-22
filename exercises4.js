b2acaeda7cbb9437f248fbd4c13dd84f5b9016ca
Rogger <rogger.vasquez@gmail.com>
2020-06-22 16:49:22 -0600

    Added the exercise for calculateGrades

--- exercises4.js
+++ exercises4.js
@@ -41,3 +41,45 @@ sumMultiples3And5(1000);
 sumMultiples3And5Typical (1000);
 // 3,6,9,12, 15 =  45
 // 5,10,15   = 30
+
+
+// Calculate grades
+
+const marks = [ 80, 80, 50 ];
+
+let result = calculateGrades(marks);
+console.log('Grade is : ', result);
+
+
+function calculateGrades ( marks) {
+   
+    // let sumOfGrades = 0;
+    // for(let grade of  marks) {
+    //     sumOfGrades += grade;
+    // }
+    // let average = sumOfGrades / marks.length ;   // Extract to average function
+    let average = calculateAverage (marks);
+
+    // Even though this conditions can be simplified, meaning that we could as just < 59 and the next < 70 and so on
+    // because we are returning when a condition is met, it makes it clearly to read the code (business rules in this case)
+    if (average >=0 && average <=59 )
+        return 'F';
+    if (average >= 60 && average <= 69  )
+        return 'D';
+    if (average >= 70 && average <= 79  )
+        return 'C';
+    if (average >= 80 && average <= 89  )
+        return 'B';
+    if (average >= 90 && average <= 100  )
+        return 'A';
+
+}
+
+function calculateAverage (elements) {
+    let sumOfElements = 0;
+    for(let item of  elements) {
+        sumOfElements += item;
+    }
+    let average = sumOfElements / marks.length ; 
+    return average;
+}
