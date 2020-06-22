e115e25c3bb4e7ebabeaf05bccbf3d932a976667
Rogger <rogger.vasquez@gmail.com>
2020-06-21 20:31:35 -0600

    Separated 3 was of solving the countTruthy function

--- exercises2.js
+++ exercises2.js
@@ -1,14 +1,20 @@
 
 const myArray = ['test', null, undefined, 0, '' , NaN,  15 , false,'333'];
-const count = countTruthy(myArray);
+const count = countTruthyV3(myArray);
 console.log ('Count of truthy is', count);
 
-function countTruthy (elements) {
+function countTruthyV1 (elements) {
     let count  = 0;
     for (let i = 0; i < elements.length ; i++) {
-        //let isTruthy = !isFalsy (elements[i]);
-        //count = isTruthy ? count + 1 : count + 0;  // Not the best way, javascript already does that
-       
+        let isTruthy = !isFalsy (elements[i]);   // did my own function, was not necessary
+        count = isTruthy ? count + 1 : count + 0; 
+    }
+    return count;
+}
+
+function countTruthyV2 (elements) {
+    let count  = 0;
+    for (let i = 0; i < elements.length ; i++) {  // used traditional for loop
         if (elements[i]) { // it will automatically evaluate if truthy ... nice!!
            count++;
         }   
@@ -16,7 +22,17 @@ function countTruthy (elements) {
     return count;
 }
 
-// This is not the best way. Javascript already does that , but was good as exercise to know the isNaN function
+function countTruthyV3 (elements) {
+    let count  = 0;
+    for (let element of elements) {  // used es6 of loop
+        if (element) { // it will automatically evaluate if truthy ... nice!!
+           count++;
+        }   
+    }
+    return count;
+}
+
+// This is not the best way. Javascript already does that , but was good as exercise to recall the isNaN function
 function isFalsy (item) {
     if (typeof item === 'undefined')
         return true;
