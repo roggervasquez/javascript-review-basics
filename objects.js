ad6189c21f2d96b568c9353b94009dd76da9195c
Rogger <rogger.vasquez@gmail.com>
2020-06-23 20:00:34 -0600

    Adding the objects file, object concepts, constructor, reference types etc

--- objects.js
+++ objects.js
@@ -52,3 +52,59 @@ function Rectangle (height, width){
 
 const rectangle1 = new Rectangle(10,20);
 rectangle1.draw();
+// add properties dynamically
+rectangle1.newProperty = 'something';
+console.log(rectangle1);
+delete rectangle1.newProperty;
+console.log(rectangle1);
+
+// rectangle1 = new Rectangle(1,1);  // this will generate an error, rectangle1 is Const, 
+                                  // so it cannot be assigned but we can actually modify its internals
+
+
+// Object and function constructors that javascript already provides
+
+let obj = {} ; // Is equivalent internally to  let obj = new Object();
+console.log(obj.constructor); // Shows that function used to create the object
+
+// Under the hood Javascript basically uses Call for a function passing the arguments
+const r = Rectangle.call({}, 2,3); // height =2. width = 3
+console.log(r);
+// or
+const r2 = Rectangle.apply({} , [2,3]) ; // We pass the arguments in array
+console.log(r2);
+
+// Primitive types copy by Value and Objects by reference
+// Value Types:  Number, String, Boolean, Symbol,  null, undefined
+// Reference Types : Object, Function, Array
+let x = 10;
+let y = x ;
+x = 20 ;
+console.log (x,y) ;  // Copied by value, there are independent
+
+let x1 = { value:10 };
+let y1 = x1 ;
+x1.value = 20 ;
+console.log (x1,y1) ;  // Copied by Reference, points to same address of memory, both will print 20
+
+// Same as passing in as arguments to a function 
+let number = 10;
+function increment(number) {  // Receive as a value parameter
+    number++;
+}
+increment(number); // Pass number as argument
+console.log(number); // will still print 10
+
+let number1 =  { value:10 };
+function increment1(number) {  // Receive as a referece parameter
+    number.value++;
+}
+increment1(number1); // Pass number as argument
+console.log(number1); // will print 11
+
+
+
+
+
+
+
