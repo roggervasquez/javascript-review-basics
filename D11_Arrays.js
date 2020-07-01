cbf4a63be03a824e099250cd19d038e0bbf6f23b
Rogger <rogger.vasquez@gmail.com>
2020-06-30 21:17:07 -0600

    Find items in arrays of objects

--- D11_Arrays.js
+++ D11_Arrays.js
@@ -18,5 +18,46 @@ console.log (localArray.indexOf('A')) ; // will  find it , so will return 2
 console.log (localArray.indexOf('A',3)) ; // will  find starting at position 3, it will return -1. A is not in that section
 console.log (localArray.lastIndexOf(5)) ; // will  will return 10, there are 2 5s stored in the array, the last one is in pos 10
 
+// To check if a primitive exists in the array
+console.log(localArray.includes ('B')); // returns true
+
+// Finding in objects
+const jobs = [
+    { id: 1 , title: 'Front end developer'} ,
+    { id: 2 , title: 'Senior Developer' },
+    { id: 3 , title: 'Senior react Developer' },
+    { id: 4 , title: 'C++ Developer' },
+    { id: 5 , title: 'nodejs Developer' },
+];
+
+let result = jobs.find(function(job) {
+    return job.title === 'nodejs Developer';
+})
+console.log(result);
+
+// or better use arrow functions, when just a parameter and just one return statement if can be abbreviated
+result = jobs.find( job =>  job.title === 'nodejs Developer');
+result2 = jobs.find( job =>  job.title.includes('Senior'));
+
+console.log(result2); // Will return only the first one found // from the 2 Senior jobs
+
+/*
+result = jobs.find( (job) => {
+    return job.title === 'nodejs Developer';
+});
+*/
+
+console.log(result);
+
+// We can also get the index of the object in the array using findIndex
+let index = jobs.findIndex( (job) => {
+    return job.title === 'nodejs Developer';
+});
+
+console.log('Index is', index);
+
+
+
+
 
 
