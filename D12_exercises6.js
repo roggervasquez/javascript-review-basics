62fe8bc812824bfc619216c99affaf8b3245c651
Rogger <rogger.vasquez@gmail.com>
2020-08-02 18:32:13 -0600

    Adding basic object exercise for Factory and constructor funcion and intro to class

---
+++ D12_exercises6.js
@@ -0,0 +1,47 @@
+// Factory and contructor function and test for EQUAL and SAME
+
+
+// Factory function
+function createAddress (street, city, zipCode) {
+    return {
+        street : street,
+        city : city,
+        zipCode : zipCode
+    }
+}
+
+// Constructor function
+function Address (street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+// Or in ES5 and on
+class AddressClass {
+    constructor(street, city, zipCode) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
+}
+
+function areEqual (address1, address2) {
+  
+    return address1.street === address2.street &&
+            address1.city === address2.city &&
+            address1.zipCode === address2.zipCode;
+}
+
+function areSame (address1, address2) {
+    return address1 === address2;
+}
+
+let addr1 = new Address ('street 1', 'miami', '31101');
+let addr2 = new Address ('street 1', 'miami', '31101');
+let addr3 = addr1;
+
+console.log (areEqual(addr1,addr2));
+console.log (areSame(addr1,addr2));
+console.log (areSame(addr1,addr3));
+
+
