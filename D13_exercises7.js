bdbc84df0995aa00d781dbc2e0402e87d14305df
Rogger <rogger.vasquez@gmail.com>
2020-08-08 21:06:28 -0600

    Using filter, sort, reverse and map in array exercise

--- D13_exercises7.js
+++ D13_exercises7.js
@@ -48,3 +48,42 @@ function countOcurrencesUsingReduce (array , searchElement) {
         return accumulator + ocurrence;
     }, 0); // Important to set the initial value of the accumulator
 }
+
+
+// Getting max value using reduce
+
+console.log ('Max Value using reduce', getMax (numbers));
+
+function getMax (array) {
+    return array.reduce ((accumulator, current) => {
+    //    if (current > accumulator ) return current;
+    //    else return accumulator;
+       return (current > accumulator) ? current : accumulator;
+
+    }) ; // No need to set accumulator because we will need first element as it is
+}
+
+// Using  filter, map, sort in arrays
+// Get all movies in 2018 with rating greater than 4 sorted in descending order and only project title and rating
+const movies = [
+ { title: 'Movie 1', year : 2018, rating : 4.5},
+ { title: 'Movie 2', year : 2018, rating : 4.5},
+ { title: 'Movie 3', year : 2017, rating : 4.3},
+ { title: 'Movie 4', year : 2018, rating : 4.4},
+ { title: 'Movie 5', year : 2018, rating : 4.9},
+ { title: 'Movie 6', year : 2020, rating : 3.5},
+ { title: 'Movie 7', year : 2018, rating : 2.5}
+]
+
+const moviesResult = movies
+.filter ( movie => movie.year === 2018 && movie.rating > 4 )
+.sort ((movie1 , movie2 ) => movie1.rating - movie2.rating)
+.reverse()
+.map ( ( {title, rating}) => ( { title,rating}));    // using object destructuring
+
+// .map (movie => {    // using simple creation of new object
+//     let mov = { title : movie.title, rating : movie.rating};
+//     return mov;
+// });
+
+console.log ('Movies Result',moviesResult);
